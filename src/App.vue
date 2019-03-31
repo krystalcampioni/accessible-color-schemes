68d7d7add9e0f73053427742da71189b2724433d
krystalcampioni <hello@krystalcampioni.com>
2019-03-31 15:01:40 +0200

    fix memory leak issue, use method instead of computed

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
-  <div id="app" v-if="selectedColorScheme">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <HelloWorld :primaryColor="primaryBgColor"></HelloWorld>
+  <div id="app">
+    <HelloWorld :primaryColor="primary.text"></HelloWorld>
 
     <div class="controls">
-      <input type="color" v-model="primaryBgColor" @change="updateColors">
+      <input type="color" v-model="mainColor" @change="updateColors" />
       <div class="color-scheme color-scheme--complementary">
-        <h1>{{ primaryContrast }}</h1>
-        <h2 :style="{
-          color: primaryTextColor
-        }">{{ primaryTextColor.color }} </h2>
         <h2>{{ selectedColorScheme.name }} Colors</h2>
         <ul>
           <li v-for="n in 4" :key="n">some text</li>
         </ul>
       </div>
       <div class="controls__buttons">
+        <button :class="{
+            '-is-selected': mode === 'dark'
+          }"
+          @click="selectMode('dark')">dark mode</button>
+        <button :class="{
+            '-is-selected': mode === 'light'
+          }"
+          @click="selectMode('light')">light mode</button>
+
         <button
           v-for="scheme in colorSchemes"
           @click="selectColorScheme(scheme)"
@@ -36,15 +37,21 @@
 </template>
 
 <script>
+const MIN_CONTRAST = 4.5;
 import Color from "color";
 import contrast from "get-contrast";
 
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
   data() {
     return {
-      primaryBgColor: "#00fbc8",
+      mainColor: "#00fbc8",
+      primary: {
+        text: "",
+        bg: ""
+      },
+      mode: "dark",
       colorSchemes: {
         complementary: {
           name: "Complementary",
@@ -71,91 +78,69 @@ export default {
     };
   },
   components: {
-    HelloWorld,
+    HelloWorld
   },
-  computed: {
-    primaryContrast() {
-      const color = Color(this.primaryBgColor);
-      const color2 = Color(this.primaryTextColor);
-      return contrast.score(this.primaryBgColor, this.primaryTextColor);
-    },
-    secondaryBgColor() {
-      const color = Color(this.primaryBgColor);
-      return color.rotate(this.selectedColorScheme.angles[0]);
-    },
-    tertiaryBgColor() {
-      const color = Color(this.primaryBgColor);
-      return color.rotate(this.selectedColorScheme.angles[1]);
-    },
-    quaternaryBgColor() {
-      const color = Color(this.primaryBgColor);
-      return color.rotate(this.selectedColorScheme.angles[2]).string();
-    },
-    primaryTextColor() {
-      const bgColor = Color(this.primaryBgColor);
-      let textColor = Color(this.primaryBgColor);
-
-      if (bgColor.luminosity() < 0.03) {
-        return "white";
-      }
-      if (bgColor.luminosity() > 0.8) {
-        return "black";
-      }
-
-      if (bgColor.isLight()) {
-        console.log('is Light')
-        textColor = this.darkenText(textColor, bgColor);
-      } else {
-        console.log('is Dark')
-        textColor = this.lightenText(textColor, bgColor);
-      }
-
-      return textColor.string();
-    }
-  },
+  computed: {},
   methods: {
+    generateTextAndBg(color) {
+      let bg = Color(color);
+      let text = bg.isLight() ? Color("#000") : Color("#fff");
+
+      // If the contrast is not enough with black or white text,
+      // we need to darken/lighten the background color
+      if (text.contrast(bg) <= MIN_CONTRAST) {
+        bg = text.isLight() ? bg.darken(0.5) : bg.lighten(0.5);
+      }
+
+      return {
+        text: text.string(),
+        bg: bg.string()
+      };
+    },
     selectColorScheme(scheme) {
       this.selectedColorScheme = scheme;
       this.updateColors();
     },
+    selectMode(mode) {
+      this.mode = mode;
+
+      this.updateColorVariable(
+        "--mode-text-color",
+        this.mode === "dark" ? "white" : "black"
+      );
+      this.updateColorVariable(
+        "--mode-bg-color",
+        this.mode === "dark" ? "black" : "white"
+      );
+    },
     updateColorVariable(colorName, newValue) {
       document.documentElement.style.setProperty(colorName, newValue);
     },
-    darkenText(color1) {
-      let temp = Color(color1);
-      let contrast = temp.contrast(color1);
-
-      while (contrast < 5 && temp.hex() !== "#FFFFFF" && temp.hex() !== "#000000") {
-        temp = temp.darken(.1);
-        contrast = temp.contrast(color1);
-      }
-      return temp;
-      // if (color1.contrast(color2) > 5) {
-      //   return color1;
-      // }
-      // return color1.darken(5);
-    },
-    lightenText(color1) {
-      let temp = Color(color1);
-      let contrast = temp.contrast(color1);
-
-      while (contrast < 15 && temp.hex() !== "#FFFFFF" && temp.hex() !== "#000000") {
-        temp = temp.lighten(.2);
-        contrast = temp.contrast(color1);
-      }
-      return temp;
-    },
     updateColors() {
-      /* eslint-disable no-inner-declarations */
-      this.updateColorVariable("--primary-bg-color", this.primaryBgColor);
-      //
-      this.updateColorVariable("--primary-text-color", this.primaryTextColor);
-      this.updateColorVariable("--secondary-bg-color", this.secondaryBgColor);
-      this.updateColorVariable("--tertiary-bg-color", this.tertiaryBgColor);
-      this.updateColorVariable("--quaternary-bg-color", this.quaternaryBgColor);
+      this.primary = this.generateTextAndBg(this.mainColor);
+      this.secondary = this.generateTextAndBg(
+        Color(this.mainColor).rotate(this.selectedColorScheme.angles[0])
+      );
+      this.tertiary = this.generateTextAndBg(
+        Color(this.mainColor).rotate(this.selectedColorScheme.angles[1])
+      );
+      this.quaternary = this.generateTextAndBg(
+        Color(this.mainColor).rotate(this.selectedColorScheme.angles[2])
+      );
+      this.updateColorVariable("--primary-text-color", this.primary.text);
+      this.updateColorVariable("--primary-bg-color", this.primary.bg);
+
+      this.updateColorVariable("--secondary-text-color", this.secondary.text);
+      this.updateColorVariable("--secondary-bg-color", this.secondary.bg);
+
+      this.updateColorVariable("--tertiary-text-color", this.tertiary.text);
+      this.updateColorVariable("--tertiary-bg-color", this.tertiary.bg);
+
+      this.updateColorVariable("--quaternary-text-color", this.quaternary.text);
+      this.updateColorVariable("--quaternary-bg-color", this.quaternary.bg);
     }
   },
-  mounted() {
+  created() {
     this.selectedColorScheme = this.colorSchemes.analogous;
     this.updateColors();
   }
@@ -164,5 +149,5 @@ export default {
 
 
 <style lang="scss">
-  @import './styles/main.scss';
+@import "./styles/main.scss";
 </style>
